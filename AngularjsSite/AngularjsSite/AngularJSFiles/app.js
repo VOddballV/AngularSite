b700b2e55f9796d20389b82cdaa4bd624759d381
Jon Carder <jcarder@access411.com>
2015-07-27 12:58:32 -0400

    Adding AngularJS website to github

---
+++ AngularjsSite/AngularjsSite/AngularJSFiles/app.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    var app = angular.module('newSite', ['ui.router', 'ngAnimate']);
+    app.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'AngularJSFiles/View/home.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'AngularJSFiles/View/about.html'
+            })
+    });
+
+    app.controller('StoreController', function () {
+        this.products = items;
+    });
+    var items = [
+        {
+            name: 'name1'
+        },
+        {
+            name: 'name2'
+        },
+        {
+            name: 'name3'
+        },
+        {
+            name: 'name4'
+        },
+        {
+            name: 'name5'
+        },
+        {
+            name: 'name6'
+        },
+        {
+            name: 'name7'
+        }
+    ];
+})();
+
+
---
+++ AngularjsSite/AngularjsSite/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngularjsSite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngularjsSite")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cb9c1627-4ad6-4861-aa26-93d1544ec250")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
